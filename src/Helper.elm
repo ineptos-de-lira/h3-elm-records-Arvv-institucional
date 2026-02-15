414b6393e2b3eca40d30a8eb36240ffc8a129801
Arvv-Institucional <up240589@alumnos.upa.edu.mx>
2026-02-14 23:44:11 -0600

    Homework

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,70 @@
 module Helper exposing (..)
+
+import Html
+
+
+add2 : Int -> Int -> Int
+add2 int1 int2 =
+    int1 + int2
+
+
+add3 : Float -> Float -> Float -> Float
+add3 int1 int2 int3 =
+    int1 + int2 + int3
+
+
+calc : Int -> Int -> (Int -> Int -> Int) -> Int
+calc int1 int2 operator =
+    operator int1 int2
+
+
+onlyStudents : List { name : String, uType : String } -> List String
+onlyStudents list =
+    List.map isStudent list
+
+
+isStudent : { name : String, uType : String } -> String
+isStudent juan =
+    if .uType juan == "Student" then
+        .name juan
+
+    else
+        ""
+
+
+type alias Videogame =
+    { title : String, releaseYear : Int, available : Bool, downloads : Int, genres : List String }
+
+
+getVideogameGenre : List Videogame -> List (List String)
+getVideogameGenre gameList =
+    List.map .genres gameList
+
+type alias Computer =
+    { ram : String
+    , model : String
+    , brand : String
+    , screenSize : String
+    }
+
+myLaptop : Computer
+myLaptop =
+    { ram = "16GB"
+    , model = "XPS 15"
+    , brand = "Dell"
+    , screenSize = "15.6"
+    }
+
+main : Html.Html msg
+main =
+  Html.div []
+  [ Html.h1 [] [ Html.text "My laptop" ]
+  , Html.div []
+    [ Html.ul []
+      [ Html.li [] [ Html.text ("Ram: " ++ .ram myLaptop) ]
+      , Html.li [] [ Html.text ("Modelo: " ++ .model myLaptop) ]
+      , Html.li [] [ Html.text ("Marca: " ++ .brand myLaptop) ]
+      , Html.li [] [ Html.text ("Pulgadas: " ++ .screenSize myLaptop) ]
+      ]
+    ]
+  ]
